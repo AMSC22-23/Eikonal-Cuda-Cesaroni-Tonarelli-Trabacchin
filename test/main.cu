88e41adce69b1254ceff93a07c0098e937217141
Melanie <melanie.tonarelli@mail.polimi.it>
2024-02-13 14:08:19 +0100

    some reorganization of code

--- test/main.cu
+++ test/main.cu
@@ -1,12 +1,11 @@
-//
-// Created by Melanie Tonarelli on 12/02/24.
-//
 #include "MultiDomainMesh.cu"
 #include "TetrahedricalMesh.cu"
-int main() {
-    TetrahedricalMesh<3> mesh("../test/meshes/input_meshes/cube-20.vtk");
 
+int main() {
+    Mesh<3> mesh("../test/meshes/input_meshes/cube-20.vtk");
     MultiDomainMesh<3> mdmesh;
     int* sol = mdmesh.partition_mesh(&mesh, 5);
     mdmesh.getSolutionsVTK(mesh, "../test/meshes/output_meshes/cube-20.vtk", sol);
+
+    return 0;
 }
