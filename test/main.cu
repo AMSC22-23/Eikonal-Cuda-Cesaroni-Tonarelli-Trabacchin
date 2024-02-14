b6b8fb8d5e9375c7768cfd015c40240cee3e8cef
Melanie <melanie.tonarelli@mail.polimi.it>
2024-02-14 09:38:25 +0100

    new version of getSolutionVTK

--- test/main.cu
+++ test/main.cu
@@ -1,10 +1,11 @@
-#include "MultiDomainMesh.cu"
+#include "Mesh.cu"
 
 int main() {
     Mesh<3> mesh("../test/meshes/input_meshes/cube-20.vtk");
-    MultiDomainMesh<3> mdmesh;
-    int* sol = mdmesh.partition_mesh(&mesh, 5);
-    mdmesh.getSolutionsVTK(mesh, "../test/meshes/output_meshes/cube-20.vtk", sol);
-
+    int* sol;
+    sol = new int[mesh.getNumberVertices()];
+    memset(sol, 0, mesh.getNumberVertices()* sizeof(int));
+    mesh.getSolutionsVTK("../test/meshes/output_meshes/cube-20-original.vtk", sol);
+    mesh.getSolutionsVTK2("../test/meshes/output_meshes/cube-20-new.vtk", sol);
     return 0;
 }
