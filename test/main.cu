37ad1d644eddc2dc306cecce4bd121dbffae8c15
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-02-15 13:22:14 +0100

    fixed uncontiguos partitions

--- test/main.cu
+++ test/main.cu
@@ -6,6 +6,15 @@ int main() {
     int* sol;
     sol = new int[mesh.getNumberVertices()];
     memset(sol, 0, mesh.getNumberVertices()* sizeof(int));
+
+    int step = 0;
+    for(int i = 0; i < mesh.getNumberVertices(); i++) {
+        if((step+1)<mesh.partitions.size() && i >= mesh.partitions[step + 1]) {
+            step++;
+        }
+        sol[i] = step;
+    }
     mesh.getSolutionsVTK("../test/meshes/output_meshes/cube-20-new.vtk", sol);
+
     return 0;
 }
