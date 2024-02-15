f4f77ef406d2cbe8a8298f098c1f50196b6021a5
Melanie <melanie.tonarelli@mail.polimi.it>
2024-02-15 13:30:07 +0100

    little modification in the code

--- test/main.cu
+++ test/main.cu
@@ -6,14 +6,14 @@ int main() {
     int* sol;
     sol = new int[mesh.getNumberVertices()];
     memset(sol, 0, mesh.getNumberVertices()* sizeof(int));
-
-    int step = 0;
-    for(int i = 0; i < mesh.getNumberVertices(); i++) {
-        if((step+1)<mesh.partitions.size() && i >= mesh.partitions[step + 1]) {
-            step++;
+    for(int i = 0; i < mesh.partitions.size(); i++){
+        int begin = mesh.partitions[i];
+        int end = (i == mesh.partitions.size() - 1) ? mesh.getNumberVertices() : mesh.partitions[i+1];
+        for (int j = begin; j < end; j++){
+            sol[j] = i+1;
         }
-        sol[i] = step;
     }
+
     mesh.getSolutionsVTK("../test/meshes/output_meshes/cube-20-new.vtk", sol);
 
     return 0;
