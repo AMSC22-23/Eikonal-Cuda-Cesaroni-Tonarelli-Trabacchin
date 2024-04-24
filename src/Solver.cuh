4d75baa150131f5d5544250e1d86d58bc43cbd78
Sabrina Cesaroni <sabrina.cesaroni@mail.polimi.it>
2024-04-24 15:08:42 +0200

    debugging

--- src/Solver.cuh
+++ src/Solver.cuh
@@ -15,9 +15,11 @@ public:
         gpuDataTransfer();
     }
 
-    static bool cudaCheck(cudaError_t err) {
+    static bool cudaCheck(cudaError_t err, std::string mess) {
         if(err != CUDA_SUCCESS) {
             std::cout << "cuda err = " << err << std::endl;
+            std::cout << "mess = "<< mess << std::endl;
+
             exit(1);
         }
     }
@@ -129,8 +131,8 @@ private:
         int numBlocksSources = source_nodes.size() / SIZE_WARP + 1;
         cudaMemset(active_domains_dev, 0, sizeof(int) * mesh->getPartitionsNumber());
         setSolutionsSourcesAndDomains<<<SIZE_WARP, numBlocksSources>>>(solutions_dev, source_nodes_dev, active_domains_dev, partitions_vertices_dev, mesh->getPartitionsNumber(), source_nodes.size());
-        cudaCheck(cudaDeviceSynchronize());
-        cudaCheck(cudaFree(source_nodes_dev));
+        cudaCheck(cudaDeviceSynchronize(), "kernel");
+        cudaCheck(cudaFree(source_nodes_dev), "free");
     }
 
 };
