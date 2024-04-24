eeb5c328310a2c089ee4107148844163da11cc74
Sabrina Cesaroni <sabrina.cesaroni@mail.polimi.it>
2024-04-24 15:06:25 +0200

    debugging

--- src/Solver.cuh
+++ src/Solver.cuh
@@ -130,7 +130,7 @@ private:
         cudaMemset(active_domains_dev, 0, sizeof(int) * mesh->getPartitionsNumber());
         setSolutionsSourcesAndDomains<<<SIZE_WARP, numBlocksSources>>>(solutions_dev, source_nodes_dev, active_domains_dev, partitions_vertices_dev, mesh->getPartitionsNumber(), source_nodes.size());
         cudaCheck(cudaDeviceSynchronize());
-        cudaCheck(cudaFree(&source_nodes_dev));
+        cudaCheck(cudaFree(source_nodes_dev));
     }
 
 };
