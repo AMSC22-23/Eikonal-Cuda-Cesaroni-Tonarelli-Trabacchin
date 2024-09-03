e7614aadc0128a3df733d669c8023583d2f743c8
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-09-03 09:36:29 +0200

    deleted old local problem folder

--- src/CudaEikonalTraits.cuh
+++ src/CudaEikonalTraits.cuh
@@ -1,13 +1,8 @@
 #ifndef EIKONAL_CUDA_CESARONI_TONARELLI_TRABACCHIN_CUDAEIKONALTRAITS_CUH
 #define EIKONAL_CUDA_CESARONI_TONARELLI_TRABACCHIN_CUDAEIKONALTRAITS_CUH
-//#include "EikonalAlgebra.cuh"
 #include <Eigen/Core>
 template <typename Float, size_t D>
 struct CudaEikonalTraits {
-    /*using VectorExt = typename EikonalAlgebra::Vec<Float, D>;
-    using VectorV = typename EikonalAlgebra::Vec<Float, D + 1>;
-    using Matrix = typename EikonalAlgebra::Matrix<Float, D>;
-    */
 
     using VectorExt = typename Eigen::Matrix<Float, D, 1>;
     using VectorV = typename Eigen::Matrix<Float, D + 1, 1>;
