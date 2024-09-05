9e5f08e00c299ea6b05e01ee5f4413bb489ee4a7
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-09-05 15:22:31 +0200

    fixed dependences installation script for correct METIS build

--- install_dependences.sh
+++ install_dependences.sh
@@ -6,6 +6,6 @@ cd GKlib
 make config CONFIG_FLAGS='-D BUILD_SHARED_LIBS=ON -D CMAKE_INSTALL_PREFIX=~/local'
 make install
 cd ../METIS
+sed -i '/add_library(metis ${METIS_LIBRARY_TYPE} ${metis_sources})/ s/$/\ntarget_link_libraries(metis GKlib)/' libmetis/CMakeLists.txt
 make config shared=1 cc=gcc prefix=~/local
 make install
-sed -i '/add_library(metis ${METIS_LIBRARY_TYPE} ${metis_sources})/ s/$/\ntarget_link_libraries(metis GKlib)/' libmetis/CMakeLists.txt
